03ccb200f99c5638f7a3587af9d6bde1da27b37b
Alejandro Bedoya <alebedoyacat@unal.edu.co>
2023-10-16 18:28:56 -0500

    Pregunta corregido

--- pregunta.py
+++ pregunta.py
@@ -5,15 +5,31 @@ Limpieza de datos usando Pandas
 Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
+from datetime import datetime
 
 def clean_data():
+
+    def normalizar_fecha(fecha):
+        try:
+            # Intenta analizar la fecha en diferentes formatos
+            formatos = ['%Y/%m/%d', '%d/%m/%Y', '%y/%m/%d', '%d/%m/%y']
+            for formato in formatos:
+                try:
+                    fecha_obj = datetime.strptime(fecha, formato)
+                    return fecha_obj.strftime('%d/%m/%Y')
+                except ValueError:
+                    pass  # Formato no coincide
+            raise ValueError('Formato de fecha no reconocido')
+        except ValueError as e:
+            print(f"Error: {e}")
+            return None
 
     # Lectura de la base de datos
     df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
     df.reset_index(inplace=True,drop=True)
     
     # Checkeo de la fecha
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(normalizar_fecha)
     
     # Botando los duplicados y los faltantes
     df.dropna(axis='index',inplace=True)
